76a2060017fa5b84237134e13ecb5ffa22bcb098
Dusan-Boskovic <dusan.boskovic@ibm.com>
2021-02-20 19:54:37 -0500

    Initial Commit

---
+++ elsa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  elsa
+//
+//  Created by Dusan Boskovic on 2021-02-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ elsa/elsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  elsaApp.swift
+//  elsa
+//
+//  Created by Dusan Boskovic on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct elsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
